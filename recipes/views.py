db25d0544771f22e3822872c2b077450a7f9a54d
Antônio Vinícius de Macêdo Sousa <ViniciusMacedo4520@hotmail.com>
2023-01-16 15:13:11 -0300

    head, search, recipe, footer creation

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def contato(request):
-    return HttpResponse('<h1>Contato</h1>')
-
-
-def sobre(request):
-    return HttpResponse('<h1>Sobre</h1>')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Antônio Vinícius',
+    })
